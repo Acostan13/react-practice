abe1e0208e9575d867c37ce459cdc2379c9846e4
Acostan13 <acostan@fau.edu>
2020-01-31 11:39:34 -0500

    Using PropTypes

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import withClass from '../../../hoc/withClass'
 import Aux from '../../../hoc/Auxiliary'
 import classes from './Person.css'
+import PropTypes from 'prop-types'
 //import Radium from 'radium'
 // import styled from 'styled-components'
 //const Button = styled.button``
@@ -22,6 +23,16 @@ class Person extends Component {
             </Aux>
         )
     }
+}
+
+// Consider using these when others are using your components
+// and it might not be super clear which props your components take
+// and which kind of data goes into which prop
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    change: PropTypes.func
 }
 
 export default withClass(Person, classes.Person);
